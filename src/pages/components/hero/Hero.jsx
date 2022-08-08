5bbab366827c68919f8e9561d616530bb6a3126a
jesus1811 <jayarza1811@gmail.com>
2022-08-08 13:51:30 -0500

    ✅ add baner and information

--- src/pages/components/hero/Hero.jsx
+++ src/pages/components/hero/Hero.jsx
@@ -3,13 +3,30 @@ import styles from "./styles.module.css";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <img className={styles.banner} src="/bannerPincipal.jpg" alt="ossi banner principal" />
-      <div className={styles.content}>
-        <img className={styles.logo} src="/logo_banner.png" alt="logo ossi" />
-        <h2 className={styles.description}>SERVICIO Y SUMINISTRO INTEGRADOS</h2>
+    <>
+      <div className={styles.hero}>
+        <img className={styles.banner} src="/bannerPincipal.jpg" alt="ossi banner principal" />
+        <div className={styles.content}>
+          <img className={styles.logo} src="/logo_banner.png" alt="logo ossi" />
+          <h2 className={styles.description}>SERVICIO Y SUMINISTRO INTEGRADOS</h2>
+        </div>
       </div>
-    </div>
+      <div className={styles.contentInformation}>
+        <img src="/bg_QuienesSomos.jpg" alt="" />
+        <aside className={styles.informationAside}>
+          <h3>¿Quienes Somos?</h3>
+          <p>
+            Somos una <b>empresa Mexicana</b> especializada en <b>brindar servicios y soluciones eficientes</b> para el
+            desarrolales e Internacionales proyectos a toda la industria.
+            <b>Cumpliendo con los estandares de Seguridad, Salud y Medio Ambiente</b> que se requieren, apegados a las
+            certificaciones y Normas Nacionales e Internacionales
+          </p>
+        </aside>
+        <aside className={styles.informationAside}>
+          <img src="/Esquema_QuienesSomos.png" alt="" />
+        </aside>
+      </div>
+    </>
   );
 };
 
