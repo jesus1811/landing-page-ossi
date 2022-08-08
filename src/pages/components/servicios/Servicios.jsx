3d009d6d76673c6f360fbb454e5b4b9d7b73da0a
jesus1811 <jayarza1811@gmail.com>
2022-08-08 14:39:00 -0500

    update bug

---
+++ src/pages/components/servicios/Servicios.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Servicios = () => {
+  return (
+    <div>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+
+    </div>
+  )
+}
+
+export default Servicios
--- src/pages/index.page.jsx
+++ src/pages/index.page.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Container, Header } from "../components";
-import { Hero } from "./components";
+import { Hero, Servicios } from "./components";
 
 const Home = () => {
   return (
     <Container title="Home | Ossi" description="empresa del rubro de metalmecanica">
       <Header />
       <Hero />
+      <Servicios/>
     </Container>
   );
 };
